2351d329961ba0e1e69fbbb19fed19296e87b892
mobile.costa@gmail.com <mobile.costa@gmail.com>
2020-10-05 21:44:23 -0300

    Uso de Template e ngFor

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: String[] = [ 'Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'https://loiane.training';
+  }
 
   ngOnInit(): void {
   }
