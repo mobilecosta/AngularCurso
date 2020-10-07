a944d0c0b8d16b8bd38f1b87ba8ccfb588a1edf4
mobile.costa@gmail.com <mobile.costa@gmail.com>
2020-10-07 09:30:55 -0300

    Aula Classe e Data Binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  url: String = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
